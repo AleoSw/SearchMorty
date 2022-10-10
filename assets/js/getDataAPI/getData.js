5ee52ac42d30090105f114845a9a49ebcb9d176f
AleoSw <98068231+AleoSw@users.noreply.github.com>
2022-10-09 21:59:32 -0500

    Add files via upload

--- assets/js/getDataAPI/getData.js
+++ assets/js/getDataAPI/getData.js
@@ -1,7 +1,11 @@
 export const API_URL = 'https://rickandmortyapi.com/api/'
 
 export async function getData(type) {
-    const API = await fetch(`${API_URL}${type}`)
-    const DATA = await API.json()
-    return DATA
+    try {
+        const API = await fetch(`${API_URL}${type}`)
+        const DATA = await API.json()
+        return DATA
+    } catch (error) {
+        console.log(`Error: ${error} : ${type}`);
+    }
 }
--- assets/js/getDataAPI/preContent.js
+++ assets/js/getDataAPI/preContent.js
@@ -1,13 +1,13 @@
 import { API_URL } from "./getData.js";
 import { getData } from "./getData.js";
 import { renderData } from "../render/renderData.js";
-
-let allData = await getData(`character`);
+import { type } from '../main.js'
 
 export async function getDataPreview() {
+    let allData = await getData(type);
     const min = 1;
     const max = allData.info.pages;
     const numRandom = Math.round(Math.random() * (max - min) + min);
-    let data = await getData(`character/?page=${numRandom}`)
+    let data = await getData(`${type}/?page=${numRandom}`)
     renderData(data.results)
 }
